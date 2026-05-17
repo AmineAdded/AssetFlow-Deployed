924570c9d9a21d8fa6a06bf6d2aec3449af4c683
Amine Added <amineadded3@gmail.com>
2026-05-17 18:22:29 +0100

    Execute ResetToken Migration

---
+++ src/Backend/AssetFlow.Infrastructure/Migrations/20260517172128_AddPasswordResetToken.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Utilisateurs",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetTokenExpiry",
+                table: "Utilisateurs",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Utilisateurs");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenExpiry",
+                table: "Utilisateurs");
+        }
+    }
+}

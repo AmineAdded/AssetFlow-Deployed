e328afbc8e983bbd5b177e5beace75e0bd921ed9
Amine Added <amineadded3@gmail.com>
2026-05-22 01:24:02 +0100

    change stock

--- src/Frontend/AssetFlow.BlazorUI/Pages/IT/Stock.razor.cs
+++ src/Frontend/AssetFlow.BlazorUI/Pages/IT/Stock.razor.cs
@@ -15,6 +15,7 @@ namespace AssetFlow.BlazorUI.Pages.IT
         [Inject] private ILocalStorageService LocalStorage  { get; set; } = default!;
         [Inject] private IJSRuntime JS { get; set; } = default!;
         [Inject] private AssetFlow.BlazorUI.Services.ArticleService ArticleSvc { get; set; } = default!;
+        [Inject] private HttpClient Http { get; set; } = default!;
 
         private List<MaterielDto> Tous             { get; set; } = new();
         private List<MaterielDto> MaterielsFiltres { get; set; } = new();
@@ -63,8 +64,9 @@ namespace AssetFlow.BlazorUI.Pages.IT
 
         private async Task ConnecterSignalR()
         {
+            var hubUrl = Http.BaseAddress!.ToString().TrimEnd('/') + "/dashboardhub";
             _hubConnection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5235/dashboardhub", options =>
+                .WithUrl(hubUrl, options =>
                 {
                     options.AccessTokenProvider = async () =>
                     {
